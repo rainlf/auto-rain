4d5e6468a72b040383ff823cc3b4c291d70ca9d1
hy何雨(IT) <yhee@trip.com>
2024-09-03 17:29:04 +0800

    update

--- src/app.py
+++ src/app.py
@@ -6,7 +6,6 @@ from config.yaml_config import YamlConfig
 import utils.file_utils as file_utils
 
 
-
 print(file_utils.get_file_path('test.yml'))
 config = YamlConfig(file_utils.get_file_path('test.yml'))
 print(config.data)
